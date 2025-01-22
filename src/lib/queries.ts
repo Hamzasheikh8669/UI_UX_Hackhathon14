2a8b58e1e432c166389cca41af4b9caa53f772c8
Hamza sheikh866 <Hamzasheikh8666@gmail.com>
2025-01-23 02:48:47 +0500

    display sanity data

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -1,5 +1,5 @@
 import { groq } from "next-sanity";
 
 export const allProducts = groq`*[_type == "product"]`;
-export const eight = groq`*[_type == "product"][0..7] `;
+export const eight = groq`*[_type == "product"][0..8] `;
 export const sixteen = groq`*[_type == "product"][12..27] `;
