a326f9c57f2b3edeb9b080447b51e41cb3fcd49c
Hamza sheikh866 <Hamzasheikh8666@gmail.com>
2025-01-23 03:12:57 +0500

    display sanity Data

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -8,5 +8,5 @@ export const client = createClient({
   dataset,
   apiVersion,
   
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: true,
 })
