04ae3761a99cef6891b94d6d5b371b682ee933c0
Hamza sheikh866 <Hamzasheikh8666@gmail.com>
2025-01-28 02:54:39 +0500

    Shop page edit

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -91,7 +91,7 @@ function ProductSection() {
       <div className="container mx-auto px-4 py-6">
         <header
           className="relative bg-cover bg-center h-64"
-          style={{ backgroundImage: "url('/images/shop.jpg')" }}
+          style={{ backgroundImage: "url('/shop.jpg')" }}
         >
           <div className="absolute inset-0 bg-opacity-50"></div>
         </header>
@@ -209,7 +209,7 @@ function ProductSection() {
           </button>
         </div>
       </div>
-      <FeatureSection />
+      <FeatureSection/>
     </div>
   );
 }
